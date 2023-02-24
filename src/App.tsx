811bb13a31166ee7d3c22636cb890d0646f77583
Running water <alsgks1218@naver.com>
2023-02-24 23:50:46 +0900

    기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scene">
+      <div className="carousel">
+        <div className="carousel__cell">1</div>
+        <div className="carousel__cell">2</div>
+        <div className="carousel__cell">3</div>
+        <div className="carousel__cell">4</div>
+        <div className="carousel__cell">5</div>
+        <div className="carousel__cell">6</div>
+        <div className="carousel__cell">7</div>
+        <div className="carousel__cell">8</div>
+        <div className="carousel__cell">9</div>
+      </div>
     </div>
   );
 }
