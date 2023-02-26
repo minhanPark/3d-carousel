5081795b6a9b2f0e80fc62ee7d6f45c1bc7a5c18
Running water <alsgks1218@naver.com>
2023-02-26 22:55:22 +0900

    이전버튼 다음버튼 구현함

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
+  const cellCount = 9;
+  const theta = 360 / cellCount;
+  const cellSize = 210;
+  // 2 * Math.PI는 360도 이다.
+  // 여기에 각 항에 cellCount을 나눈다
+  // (2 * Math.PI) / cellCount = 360 / cellCount
+  // 여기에 우리는 직각을 통해 탄젠트를 사용할 것이므로 나오는 값을 반으로 나눠야 한다.
+  // 그래서 다시 2를 또 나눈다
+  // ((2 * Math.PI) % cellCount) % 2  = (360 / cellCount) % 2
+  // 위의 식을 줄인게 Math.tan( Math.PI / cellCount)
+  const radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount));
+  const boxex = Array.from({ length: 9 }, (v, i) => i + 1);
+  const rotateCarousel = (selectedIndex: number) => {
+    const angle = theta * selectedIndex * -1;
+    if (carousel?.current) {
+      carousel.current.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`;
+    }
+  };
+  const handlePrev = () => {
+    setSelectedIndex((prevIndex) => {
+      rotateCarousel(prevIndex - 1);
+      return prevIndex - 1;
+    });
+  };
+  const handleNext = () => {
+    setSelectedIndex((prevIndex) => {
+      rotateCarousel(prevIndex + 1);
+      return prevIndex + 1;
+    });
+  };
   return (
-    <div className="scene">
-      <div className="carousel">
-        <div className="carousel__cell">1</div>
-        <div className="carousel__cell">2</div>
-        <div className="carousel__cell">3</div>
-        <div className="carousel__cell">4</div>
-        <div className="carousel__cell">5</div>
-        <div className="carousel__cell">6</div>
-        <div className="carousel__cell">7</div>
-        <div className="carousel__cell">8</div>
-        <div className="carousel__cell">9</div>
+    <>
+      <div className="scene">
+        <div className="carousel" ref={carousel}>
+          {boxex.map((value) => (
+            <div className="carousel__cell" key={value}>
+              {value}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <button onClick={handlePrev}>이전</button>
+        <button onClick={handleNext}>다음</button>
+      </div>
+    </>
   );
 }
 
