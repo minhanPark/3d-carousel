ec6ed2cfc020b14e2feeb47c1ab9abc00617eac3
Running water <alsgks1218@naver.com>
2023-02-27 21:49:36 +0900

    15개로 변경 및 state부터 바꿔봄

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import "./App.css";
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const carousel = useRef<HTMLDivElement>(null);
-  const cellCount = 9;
-  const theta = 360 / cellCount;
+  const cellCount = 15; //15
+  const theta = 360 / cellCount; // 24
   const cellSize = 210;
   // 2 * Math.PI는 360도 이다.
   // 여기에 각 항에 cellCount을 나눈다
@@ -15,25 +15,39 @@ function App() {
   // ((2 * Math.PI) % cellCount) % 2  = (360 / cellCount) % 2
   // 위의 식을 줄인게 Math.tan( Math.PI / cellCount)
   const radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount));
-  const boxex = Array.from({ length: 9 }, (v, i) => i + 1);
+  const boxex = Array.from({ length: 15 }, (v, i) => i + 1);
   const rotateCarousel = (selectedIndex: number) => {
     const angle = theta * selectedIndex * -1;
     if (carousel?.current) {
       carousel.current.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`;
     }
   };
+  // const rotate = () => {
+  //   const angle = theta * selectedIndex * -1;
+  //   if (carousel?.current) {
+  //     carousel.current.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`;
+  //   }
+  // };
   const handlePrev = () => {
     setSelectedIndex((prevIndex) => {
       rotateCarousel(prevIndex - 1);
       return prevIndex - 1;
     });
   };
+  // const handlePrev = () => {
+  //   setSelectedIndex((prevIndex) => prevIndex - 1);
+  //   rotate();
+  // };
   const handleNext = () => {
     setSelectedIndex((prevIndex) => {
       rotateCarousel(prevIndex + 1);
       return prevIndex + 1;
     });
   };
+  // const handleNext = () => {
+  //   setSelectedIndex((prevIndex) => prevIndex + 1);
+  //   rotate();
+  // };
   return (
     <>
       <div className="scene">
