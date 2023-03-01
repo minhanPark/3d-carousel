174df3f2ed64f4848cb154903f588f379f2bdb2d
Running water <alsgks1218@naver.com>
2023-03-01 16:43:56 +0900

    card flip 추가

--- src/components/CardFlip.tsx
+++ src/components/CardFlip.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./CardFlip.module.css";
 
 const CardFlip = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked((prevValue) => !prevValue);
+  };
   return (
-    <div className={style.scene}>
-      <div className={style.card}>
-        <img
-          className={style.card - face}
-          src="https://images.pokemontcg.io/swsh35/1_hires.png"
-          alt="front"
-        />
-        <img
-          className={style.card - face}
-          src="https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg"
-          alt="back"
-        />
+    <div className={style.container}>
+      <div className={style.scene}>
+        <div
+          onClick={handleClick}
+          className={`${style.card} ${clicked ? style["is-flipped"] : ""}`}
+        >
+          <img
+            className={`${style["card-face"]} ${style.back}`}
+            src="https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg"
+            alt="back"
+          />
+          <img
+            className={style["card-face"]}
+            src="https://images.pokemontcg.io/swsh35/1_hires.png"
+            alt="front"
+          />
+        </div>
       </div>
     </div>
   );
